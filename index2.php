134e4a74e2bd9ad96c371cc078e4d12062672b61
Vasundhra <vasundhragupta@gmail.com>
2014-11-02 22:08:15 +0530

    index2

---
+++ index2.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+?>
+
+<html>
+<body background= "bg.png">
+	<center>
+		<div style="z-index: -1; background: #cccccc; width: 300px; margin-top: 10%;  box-shadow: 10px 10px 5px #888888;  border-radius: 25px;">
+		<br><br>
+	<a href="batch.php" target="_blank">List of Batches</a> &nbsp&nbsp  <a href="new_batch.php" target="_blank">New Batches</a><br><br>
+	<a href="subjects.php" target="_blank">List of Subjects</a> &nbsp &nbsp  <a href="new_subjects.php" target="_blank">New Subjects</a><br><br>
+	<a href="room.php" target="_blank">List of Rooms</a> &nbsp &nbsp <a href="new_room.php" target="_blank">New Room</a><br><br>
+	<br>
+
+	</div>
+	</center>
+	
+</body>
+</html>
